da98a5dfa6e7b265ad4112065796f23dbd0dd824
Gert van Dijk <github@gertvandijk.nl>
2023-07-18 23:19:40 +0200

    Remove some unneeded Pyright suppressions

--- src/purepythonmilter/__init__.py
+++ src/purepythonmilter/__init__.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# pyright: reportUnusedImport=false
 
 from __future__ import annotations
 
--- tests/integration/test_server.py
+++ tests/integration/test_server.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# pyright: reportPrivateUsage=false
 from __future__ import annotations
 
 import logging
